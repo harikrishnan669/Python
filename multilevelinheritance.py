e18d555475c5ed4be9091ebdbc361941e67e2a73
Harikrishnan R <128880349+harikrishnan669@users.noreply.github.com>
2024-07-16 23:00:44 +0530

    Create multilevelinheritance.py

---
+++ multilevelinheritance.py
@@ -0,0 +1,17 @@
+# Multilevel inheritance
+class First():
+  def display_first(self):
+    print("First")
+
+class Second(First):
+  def display_second(self):
+    print("Second")
+
+class Third(Second):
+  def display_third(self):
+    print("Third")
+
+x=Third()
+x.display_first()
+x.display_second()
+x.display_third()
