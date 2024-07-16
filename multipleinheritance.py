b260f954378dc437ece75091378eb7c3b8a5d2e3
Harikrishnan R <128880349+harikrishnan669@users.noreply.github.com>
2024-07-16 23:00:11 +0530

    Create multipleinheritance.py

---
+++ multipleinheritance.py
@@ -0,0 +1,17 @@
+# Multiple inheritance
+class First:
+  def display_first(self):
+    print("First")
+
+class Second:
+  def display_second(self):
+    print("Second")
+
+class Third(First,Second):
+  def display_third(self):
+    print("Third")
+
+x=Third()
+x.display_third()
+x.display_second()
+x.display_first()
