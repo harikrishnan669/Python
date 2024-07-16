83956bcddefad33d4d217a9c7c75ac19d9966a36
Harikrishnan R <128880349+harikrishnan669@users.noreply.github.com>
2024-07-16 13:06:09 +0530

    Update instance_creation.py

--- instance_creation.py
+++ instance_creation.py
@@ -1,7 +1,11 @@
+#__init__ method is to initialize (or assign values) the data members of a class when an object of that class is created
+# “self” is used to access and manipulate the instance variables and methods within a class.
 class students:
   year=2020
-  def __init__(self,name,age,branch):   #__init__ method is to initialize (or assign values) the data members of a class when an object of that class is created
-    self.name=name                      # “self” is used to access and manipulate the instance variables and methods within a class.
+  def __init__(self,name,age,branch):
+    self.name=name
+    self.age=age
+    self.branch=branch
 
   def add_age(self):
     self.age=self.age+1
@@ -13,12 +17,17 @@ class students:
     print("Age "+ str(self.age))
     print("branch "+ self.branch)
 
+  @classmethod
+  def add_year(cls):
+    cls.year=cls.year+1
+
 x=students("hari",20,"cse")
 y=students("ravi",20,"cse")
 
 x.display()
 y.display()
 
+print("---------------------------------------")
 students.year=students.year+1
 
 x.add_age()
@@ -26,3 +35,11 @@ y.add_age()
 
 x.display()
 y.display()
+
+print("---------------------------------------")
+students.add_year()
+
+x.display()
+y.display()
+
+  
