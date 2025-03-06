5e7f4ff8f186896c95c8f1f3de7d9328f9db3cdc
Harikrishnan R <128880349+harikrishnan669@users.noreply.github.com>
2025-03-06 20:54:13 +0530

    Create rev.py

---
+++ rev.py
@@ -0,0 +1,12 @@
+number = int(input("Enter a 5 didgit number"))
+o=number;
+s=0;
+while number>0:
+  d=number%10
+  s=s*10+d
+  number=number//10
+print("The reversed number is", s)
+if(s==o):
+  print("Palindrome")
+else:
+  print("Not palindrome")
